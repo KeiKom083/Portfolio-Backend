6eab16a76fe3b549c4e71c87bfe130a8cc87d489
KeiKom083 <gt.chicken1@gmail.com>
2026-05-22 15:57:01 +0900

    ユーザー登録機能の改善：パスワードハッシュ化をモデルのNewUser関数に移動

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 // User はユーザーのドメインモデル。
 type User struct {
@@ -10,4 +15,17 @@ type User struct {
 	PasswordHash string
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
+}
+
+// NewUser はパスワードをハッシュ化してユーザーを生成する。
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("hash password: %w", err)
+	}
+	return &User{
+		Name:         name,
+		Email:        email,
+		PasswordHash: string(hash),
+	}, nil
 }
